49bbb0cda1b29055341735c40362b084c09b20ce
shad-bin-abi-aydid <shadaydid@gmail.com>
2026-02-26 08:26:17 +0000

    All questions are answered now

--- schema.sql
+++ schema.sql
@@ -70,6 +70,159 @@ CREATE TABLE bookings (
     CHECK (end_date >= start_date)
 );
 
+-- using Trigger to calculate total_price automatically
+-- execute function
+CREATE OR REPLACE FUNCTION calculate_total_cost () RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
 
+    DECLARE
+      daily_price NUMERIC(10,2);
+      total_days INTEGER;
+    BEGIN
 
+      -- get the price perday
+      SELECT price_per_day INTO daily_price FROM vehicles WHERE id = NEW.vehicle_id;
 
+      -- Calculate total days
+      total_days := (NEW.end_date - NEW.start_date) + 1;
+
+      -- Calculate the total cost
+      NEW.total_cost := total_days * daily_price;
+
+      RETURN NEW;
+  
+    END;
+  $$;
+
+-- ctrate trigger
+CREATE TRIGGER trg_calculate_total_cost BEFORE INSERT
+OR
+UPDATE ON bookings FOR EACH ROW
+EXECUTE FUNCTION calculate_total_cost ();
+
+-- Insert values in the users table
+INSERT INTO
+  users (role, name, email, password, phone_number)
+VALUES
+  (
+    'customer',
+    'Alice',
+    'alice@example.com',
+    'password123',
+    '1234567890'
+  ),
+  (
+    'admin',
+    'Bob',
+    'bob@example.com',
+    'password123',
+    '0987654321'
+  ),
+  (
+    'customer',
+    'Charlie',
+    'charlie@example.com',
+    'password123',
+    '1122334455'
+  );
+
+-- Insert into the vehicles table
+INSERT INTO
+  vehicles (
+    name,
+    type,
+    model,
+    registration_number,
+    price_per_day,
+    status
+  )
+VALUES
+  (
+    'Toyota Corolla',
+    'car',
+    '2022',
+    'ABC-123',
+    50,
+    'available'
+  ),
+  (
+    'Honda Civic',
+    'car',
+    '2021',
+    'DEF-456',
+    60,
+    'rented'
+  ),
+  (
+    'Yamaha R15',
+    'bike',
+    '2023',
+    'GHI-789',
+    30,
+    'available'
+  ),
+  (
+    'Ford F-150',
+    'truck',
+    '2020',
+    'JKL-012',
+    100,
+    'maintenance'
+  );
+
+-- Insert values to the bookings
+INSERT INTO
+  bookings (user_id, vehicle_id, start_date, end_date, status)
+VALUES
+  (1, 2, '2023-10-01', '2023-10-05', 'completed'),
+  (1, 2, '2023-11-01', '2023-11-03', 'completed'),
+  (3, 2, '2023-12-01', '2023-12-02', 'confirmed'),
+  (1, 1, '2023-12-10', '2023-12-12', 'pending');
+
+-- Question 1 inner join
+select
+  u.name as customer_name,
+  v.name as vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.status
+from
+  bookings as b
+  inner join users as u on b.user_id = u.id
+  inner join vehicles as v on b.vehicle_id = v.id;
+
+-- Question 2 finds all vehicle which never been booked
+select
+  *
+from
+  vehicles as v
+where
+  not exists (
+    select
+      *
+    from
+      bookings as b
+    where
+      b.vehicle_id = v.id
+  );
+
+-- Question 3 Retrieve all available vehicles of a specific type
+select
+  *
+from
+  vehicles
+where
+  type = 'car'
+  and status = 'available';
+
+-- Question 4
+select
+  v.name as vehicle_name,
+  count(*) as total_booking
+from
+  bookings as b
+  join vehicles as v on b.vehicle_id = v.id
+group by
+  b.vehicle_id,
+  v.name
+having
+  count(*) > 2;
